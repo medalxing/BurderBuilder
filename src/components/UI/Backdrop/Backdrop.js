7a027c39b43124c1234fb101fbca1655bffe2e93
Medal Xing <daojiyan@gmail.com>
2019-04-16 19:07:51 -0400

    add hamburger menu icon to toggle sidedrawer and improve performance by adding life cycle

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -3,6 +3,10 @@ import classes from "./Backdrop.css";
 
 const backdrop = (props) =>(
      props.show ? 
-    <div className={classes.Backdrop} onClick = {props.clicked}></div>: null
+    <div 
+        className={classes.Backdrop} 
+        onClick = {props.clicked}>
+        
+    </div>: null
 )
 export default backdrop;
