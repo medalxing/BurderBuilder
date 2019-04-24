a0b57381d93cd6b8d4393b82947534e6b3b58170
Medal Xing <daojiyan@gmail.com>
2019-04-24 16:26:46 -0400

    add redux-thunk and action creators

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route,Redirect ,Switch} from 'react-router-dom';
+import {Route,Switch} from 'react-router-dom';
 import Layout from "./hoc/Layout/Layout"
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
