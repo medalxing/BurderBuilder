10da14dd9732061987bad33a2825da4fec29b139
Medal Xing <daojiyan@gmail.com>
2019-04-26 14:39:14 -0400

    add authorization to orders

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -103,7 +103,8 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ings,
             price: this.props.price,
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
         }
         this.props.onOrderBurger(order,this.props.token);
         
@@ -187,6 +188,7 @@ const mapStateToProps = state =>{
         price: state.burgerBuilder.totalPrice,
         loading: state.order.loading,
         token: state.auth.token,
+        userId: state.auth.userId
     }
 }
 const mapDispatchToProps = dispatch => {
